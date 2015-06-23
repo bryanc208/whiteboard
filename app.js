1698c08be0dab530b068dbc4e90aa8f3f6495128
Bryan Cho <bryanc208@gmail.com>
2015-06-22 21:48:19 -0400

    Initial commit. Basic components, working drawing canvas and socket.io setup.

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/app'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/app/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log("[" + new Date().toUTCString()+"]" + ' New user connected');
+	socket.on('disconnect', function() {
+		console.log("[" + new Date().toUTCString()+"]" + ' User disconnected');
+	});
+
+	socket.on('chat message', function(msg){
+    	io.emit('chat message', msg);
+  	});
+})
+
+http.listen(3000, function() {
+	console.log('listening on *:3000');
+});
